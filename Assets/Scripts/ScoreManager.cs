d2b47f182ebbe49aebd893f260e72f840cb4c6d2
Spevan <67030925+Spevan@users.noreply.github.com>
2024-02-28 21:30:10 -0500

    Add files via upload

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static float score;
+    public TextMeshProUGUI scoreDisplay;
+    void Start()
+    {
+        score = 0;
+        scoreDisplay.SetText("Points: " + score);
+    }
+
+    public void scoreUpdate()
+    {
+        score++;
+        scoreDisplay.SetText("Points: " + score);
+    }
+}
